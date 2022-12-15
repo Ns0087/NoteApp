6add4cf07b73fb3a6b36bef053927034e7f69593
NS0087 <nitinsaraswat.30@gmail.com>
2022-12-15 12:51:18 +0530

    Notes App Added

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React BootCamp</h1>
+      <p>
+        All the Students who complete the BootCamp with complete attendance and
+        project submission will be rewarded certificates from us.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
